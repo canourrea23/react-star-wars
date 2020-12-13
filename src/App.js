9d9aa00519e7a4f9d1bfc0116e52def91bec26b1
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-12-13 13:29:17 -0500

    hw finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './App.css';
+import StarShipResult from './components/StarShipResult';
 import StarShips from './components/StarShips';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-       <Route path='/starship' render={({location}) => 
-          <StarShips location={location}/>
+    <header className="App-header">
+      <p>Star Wars Ships</p>
+    </header>
+    <Switch>
+       <Route exact path='/starship' render={({location}) => 
+          <StarShipResult location={location}/>
           }/>
+        <Route exact path='/' render={({location}) => 
+          <StarShips location={location}/>
+          }/></Switch>
     </div>
     </BrowserRouter>
   );
--- src/components/StarShips.js
+++ src/components/StarShips.js
@@ -1,48 +1,43 @@
 import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
 import StarShipResult from './StarShipResult';
-function StarShips(props) {
-  // TODO: Verify zip is actually a zip
-  const [num, setNum] = useState('');
-  const [starship, setStarShip] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+function StarShips(props) {
+  const [starship, setStarShip] = useState([]);
+  const getStarShip= () => {
     // We will call the API using axios
-    let url =`https://swapi.dev/api/starships/${num}`;
+    let url ='https://swapi.dev/api/starships/';
     axios.get(url)
-    .then(result => {
+    .then(res => {
+      console.log(res)
       // Set sum state
-      setStarShip(result.data)
+      setStarShip(res.data.results)
     })
     .catch(err => {
       console.log(err);
     })
   }
+  useEffect(() => {
+    getStarShip()
+  },[])
+
   return (
-   
-        props.starships.map(starship =>
-        <Link
+    <div>
+      <ul>
+        {starship.map(starship =>
+        <li>
+        <Link     
             to={{
             pathname: '/starship',
             state: starship,
             }}
             key={starship.name}>
-            {{starship.name}}
-        </Link>
-    // <div>
-    //   <form onSubmit={handleSubmit}>
-    //     <h1>Find the Star Ship</h1>
-    //     {starship ? <StarShipResult StarShip={starship}/> : null}
-    //     <label htmlFor='num'>Space craft: </label>
-    //     <input type="text" name="num" id="num" onChange={e => setNum(e.target.value)}/>
-    //     <input type="submit" value="Find SpaceShips"/>
-    //   </form>
-    // </div>
-  //)
-    )
+            {starship.name}        
+        </Link></li>)}
+      </ul>
+    </div>
+  // )
   )
 }
 export default StarShips;
